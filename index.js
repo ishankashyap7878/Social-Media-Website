48d612144511a0e5ad8ef0ec64f88b66a6442e92
Ishan Kashyap <ishank243@gmail.com>
2020-05-07 18:19:26 +0530

    added diff css for diff files

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const app=express();
 const port=8002;
 const expressLayouts=require('express-ejs-layouts');
 app.use(expressLayouts);
+
 app.use(express.static('assets'));
+
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 //use express router
 app.use('/',require('./routes'));
+
+
 //setting up the view engine
 app.set('view engine','ejs');
 app.set('views','./views');
