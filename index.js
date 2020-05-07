9f7a1cea34687956b379d44ce5940a0598f1c1dd
Ishan Kashyap <ishank243@gmail.com>
2020-05-07 17:58:56 +0530

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express= require('express');
 const app=express();
-const port=8001;
+const port=8002;
 const expressLayouts=require('express-ejs-layouts');
 app.use(expressLayouts);
+app.use(express.static('assets'));
 //use express router
 app.use('/',require('./routes'));
-app.use(express.static('assets'));
+//setting up the view engine
 app.set('view engine','ejs');
 app.set('views','./views');
+
 app.listen(port,function(err)
 {
     if(err)
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,14 @@ const express=require('express')
 
 const router=express.Router();
 
-const homeController=require('../controllers/home_controller')
-const usersController=require('../controllers/users_controller')
+const homeController=require('../controllers/home_controller');
 console.log('router loaded');
 
 
 router.get('/',homeController.home);
-router.use('/users',usersController.profile);
-
+//anything that comes for users will be forwarded to users
+router.use('/users',require('./users'));
+//for anything other
+//router.use('/routerName',require('./routerfile'));
 
 module.exports=router;
